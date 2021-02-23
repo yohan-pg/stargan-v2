710267e2459e15736a4c6437e998f2974d210316
yohan-pg <pg.yohan@gmail.com>
2021-02-22 21:27:59 -0500

    added missing train

--- batch_all_expr.sh
+++ batch_all_expr.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-sbatch train_expr_method.sh
 sbatch train_expr_block_size.sh
 sbatch train_expr_center_color_at_identity.sh
 sbatch train_expr_denman_beavers.sh
 sbatch train_expr_learn_alpha.sh
 sbatch train_expr_make_color_symmetric.sh
 sbatch train_expr_mean_shift.sh
+sbatch train_expr_method.sh
+sbatch train_expr_use_mlp.sh
