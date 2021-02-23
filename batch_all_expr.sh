e1de555c72268d947e1846658dbbb5123d8e7401
yohan-pg <pg.yohan@gmail.com>
2021-02-22 21:18:56 -0500

    ./

---
+++ batch_all_expr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+sbatch train_expr_method.sh
+sbatch train_expr_block_size.sh
+sbatch train_expr_center_color_at_identity.sh
+sbatch train_expr_denman_beavers.sh
+sbatch train_expr_learn_alpha.sh
+sbatch train_expr_make_color_symmetric.sh
+sbatch train_expr_mean_shift.sh
